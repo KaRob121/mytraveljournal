665b35af72b01781be26d609272bb756955274bd
Karl <24803299+KaRob121@users.noreply.github.com>
2022-04-14 20:24:43 -0700

    Completed project by fixing styling and adding data for other sample locations

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ function Header() {
       <img 
         className="header--logo"
         src={ earth }
+        alt="Planet Earth icon"
       />
       <h1 className="header--heading">my travel journal.</h1>
     </header>
--- src/components/JournalEntry.js
+++ src/components/JournalEntry.js
@@ -3,20 +3,23 @@ import pin from "../images/pin.png"
 function JournalEntry(props) {
   return (
     <main className="entry">
-      <img 
-        src={`${ props.entry.imageUrl }`}
+      <img
         className="entry--img"
+        src={`${ props.entry.imageUrl }`}
+        alt="Image of location visited"
       />
       <div className="entry--information">
         <div className="entry--location">
-          <span>
-            <img src={ pin }/>
-            { props.entry.location }
-            <a href={`${ props.entry.googleMapsUrl }`}>View on Google Maps</a>
-          </span>
+          <img 
+            src={ pin }
+            className="entry--pin-icon"
+            alt="Pin icon"
+          />
+          <span className="entry--location--country">{ props.entry.location }</span>
+          <a href={`${ props.entry.googleMapsUrl }`}>View on Google Maps</a>
         </div>
-        <h1>{ props.entry.title }</h1>
-        <p>{ props.entry.startDate } - { props.entry.endDate }</p>
+        <h1 className="entry--title">{ props.entry.title }</h1>
+        <p className="bold">{ props.entry.startDate } - { props.entry.endDate }</p>
         <p>{ props.entry.description }</p>
       </div>
     </main>
